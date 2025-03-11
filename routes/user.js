0e96554f631057b00c8203cf3fb3473173c91aeb
Zachery Davis <zacherydavis@hotmail.com>
2025-03-10 22:31:24 -0400

    Added Routes

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+
+router.get("/profile", (req, res) => {
+  res.render("user/profile");
+});
+
+module.exports = router;
